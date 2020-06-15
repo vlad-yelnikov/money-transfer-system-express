60820e027fcfc5ae2a42f97f94e747cb4252b3c6
KN18 <50597469+KN18@users.noreply.github.com>
2020-06-15 12:57:16 +0300

    Added user model and test "post" route

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const mongoConnect = require('./src/db/index');
 
-const routes = require('./src/routes');
+const routes = require('./src/routes/index');
 
 mongoConnect();
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(routes);
+app.use(express.json({ extended: false }));
+app.use('/api/users', routes);
+
+// app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
-const router = express.Router();
+const User = require('../models/index');
 
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
+});
+
+router.post('/', (req, res, next) => {
+  const user = new User({
+    name: req.body.name,
+  });
+  user.save();
+  res.json(user);
 });
 
 module.exports = router;
