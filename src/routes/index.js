1c2159f6bd714df5f5a4e489bd2e66aad0a9ef96
KN18 <50597469+KN18@users.noreply.github.com>
2020-06-22 00:42:15 +0300

    Added using handler in router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const users = require('./users');
 const cards = require('./cards');
+const errorHandler = require('../Middleware/ErrorHandler');
 
 const router = express.Router();
 
 router.use('/users', users);
 router.use('/cards', cards);
+
+router.use(errorHandler);
 
 module.exports = router;
