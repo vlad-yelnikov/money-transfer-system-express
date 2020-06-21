965f3a5afee2a09eb009b4230d815f87084fd96e
Vladyslav Yelnikov <31829943+vlad-yelnikov@users.noreply.github.com>
2020-06-22 00:15:44 +0300

    add card model (#11)
    * add card model
    
    * fix card model
    
    * set card number min\max, debit, credit and creditLimit min
    
    * change .gitignore
    
    * change .gitignore

--- src/models/card.js
+++ src/models/card.js
@@ -2,27 +2,37 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const card = new Schema({
-  number: {
-    type: Number,
-    required: true,
+const card = new Schema(
+  {
+    number: {
+      type: Number,
+      min: 11111111,
+      max: 99999999,
+      required: true,
+    },
+    debit: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    credit: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    creditLimit: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
   },
-  debit: {
-    type: Number,
-    required: true,
+  {
+    strict: 'throw',
   },
-  credit: {
-    type: Number,
-    required: true,
-  },
-  creditLimit: {
-    type: Number,
-    required: true,
-  },
-  user: {
-    type: String,
-    required: true,
-  },
-});
+);
 
 module.exports = mongoose.model('card', card);
