29f91e9e6c06844a2564083731c5a0868a90ca74
Anton <asavienko@gmail.com>
2020-06-22 14:37:04 +0300

    improved _formatFilter method

--- src/services/CardService.js
+++ src/services/CardService.js
@@ -15,37 +15,43 @@ class CardService extends MainService {
     creditlimitfrom,
     creditlimitto,
   }) {
-    const filter = this._formatFilter(debitfrom,
+    const filter = this._formatFilter(
+      debitfrom,
       debitto,
       creditfrom,
       creditto,
       creditlimitfrom,
-      creditlimitto);
-    return super.search({
-      page,
-      size,
-      order,
-      sort,
-    }, filter);
+      creditlimitto,
+    );
+    return super.search(
+      {
+        page,
+        size,
+        order,
+        sort,
+      },
+      filter,
+    );
   }
 
-  _formatFilter(debitfrom,
+  _formatFilter(
+    debitfrom,
     debitto,
     creditfrom,
     creditto,
     creditlimitfrom,
-    creditlimitto) {
+    creditlimitto,
+  ) {
     const rawFilter = {
       debit: _.pickBy({ $gte: debitfrom, $lte: debitto }, _.identity),
       credit: _.pickBy({ $gte: creditfrom, $lte: creditto }, _.identity),
-      creditLimit: _.pickBy({ $gte: creditlimitfrom, $lte: creditlimitto }, _.identity),
+      creditLimit: _.pickBy(
+        { $gte: creditlimitfrom, $lte: creditlimitto },
+        _.identity,
+      ),
     };
-    const arrOfFilter = Object.entries(rawFilter).map(([key, value]) => {
-      if (!_.isEmpty(value)) {
-        return [key, value];
-      }
-    }).filter((value) => !!value);
-    return Object.fromEntries(arrOfFilter);
+    const arrOfFilter = _.omitBy(rawFilter, _.isEmpty);
+    return arrOfFilter;
   }
 }
 
