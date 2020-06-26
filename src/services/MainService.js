d0abe98b7fd17eb77326e60b937551531c8b2b05
KN18 <50597469+KN18@users.noreply.github.com>
2020-06-27 02:34:50 +0300

    Added transaction search

--- src/services/MainService.js
+++ src/services/MainService.js
@@ -23,7 +23,9 @@ class MainService {
     return this.Model.findByIdAndRemove(id);
   }
 
-  search({ page = 1, size = 10, order, sort }, filter = {}) {
+  search({
+    page, size, order, sort,
+  }, filter = {}) {
     const skip = (page - 1) * size;
     return this.Model.find(filter, null, {
       limit: +size,
--- src/services/TransactionService.js
+++ src/services/TransactionService.js
@@ -1,6 +1,50 @@
+const _ = require('lodash');
 const { transaction } = require('../models');
 const MainService = require('./MainService');
 
-class TransactionService extends MainService {}
+class TransactionService extends MainService {
+  search({
+    page,
+    size,
+    order,
+    sort,
+    datefrom,
+    dateto,
+    amountfrom,
+    amountto,
+    card,
+  }) {
+    const filter = this._formatFilter(
+      datefrom,
+      dateto,
+      amountfrom,
+      amountto,
+      card,
+    );
+
+    return super.search(
+      {
+        page,
+        size,
+        order,
+        sort,
+      },
+      filter,
+    );
+  }
+
+  _formatFilter(datefrom, dateto, amountfrom, amountto, card) {
+    const rawFilter = {
+      date: _.pickBy({ $gte: datefrom, $lte: dateto }, _.identity),
+      amount: _.pickBy({ $gte: amountfrom, $lte: amountto }, _.identity),
+      $or: [
+        _.pickBy({ sender: card }, _.identity),
+        _.pickBy({ receiver: card }, _.identity),
+      ],
+    };
+
+    return _.omitBy(rawFilter, _.isEmpty);
+  }
+}
 
 module.exports = new TransactionService(transaction);
