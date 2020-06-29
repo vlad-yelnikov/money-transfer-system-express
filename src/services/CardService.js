8fcceaf7a5b8d84a5a864328cf6529db5e864e83
Vladyslav Yelnikov <31829943+vlad-yelnikov@users.noreply.github.com>
2020-06-29 14:21:27 +0300

    Increase/decrease amount (#15)
    * add routes
    
    * add increase, decrease and setLimit
    
    * some changes
    
    * complete setLimit
    
    * complete increase and decrease
    
    * replace debit to value
    
    * add throw error
    
    * prevent negative debit
    
    * remove else
    
    * add withdraw credit money
    
    Co-authored-by: KN18 <50597469+KN18@users.noreply.github.com>

--- src/services/CardService.js
+++ src/services/CardService.js
@@ -21,7 +21,7 @@ class CardService extends MainService {
       creditfrom,
       creditto,
       creditlimitfrom,
-      creditlimitto,
+      creditlimitto
     );
     return super.search(
       {
@@ -30,7 +30,7 @@ class CardService extends MainService {
         order,
         sort,
       },
-      filter,
+      filter
     );
   }
 
@@ -40,36 +40,58 @@ class CardService extends MainService {
     creditfrom,
     creditto,
     creditlimitfrom,
-    creditlimitto,
+    creditlimitto
   ) {
     const rawFilter = {
       debit: _.pickBy({ $gte: debitfrom, $lte: debitto }, _.identity),
       credit: _.pickBy({ $gte: creditfrom, $lte: creditto }, _.identity),
       creditLimit: _.pickBy(
         { $gte: creditlimitfrom, $lte: creditlimitto },
-        _.identity,
+        _.identity
       ),
     };
 
     return _.omitBy(rawFilter, _.isEmpty);
   }
 
-  increase(id, value) {
-    if (typeof value === 'number' && value > 0) {
-      return this.Model.findByIdAndUpdate(id, { $inc: { debit: value } });
+  async increase(id, value) {
+    if (value < 0) {
+      const err = new Error('Bad request');
+      err.status = 400;
+      throw err;
     }
-    const err = new Error('Bad request');
-    err.status = 400;
-    throw err;
+    const cardDoc = await this.Model.findById(id);
+    if (!cardDoc) return;
+    if (cardDoc.credit < value) {
+      cardDoc.debit += value - cardDoc.credit;
+      cardDoc.credit = 0;
+      cardDoc.save();
+      return cardDoc;
+    }
+    cardDoc.credit -= value;
+    cardDoc.save();
+    return cardDoc;
   }
 
-  decrease(id, value) {
-    if (typeof value === 'number' && value > 0) {
-      return this.Model.findByIdAndUpdate(id, { $inc: { debit: -value } });
+  async decrease(id, value) {
+    const cardDoc = await this.Model.findById(id);
+    if (!cardDoc) return;
+    const notEnoughMoney =
+      value > cardDoc.debit + cardDoc.creditLimit - cardDoc.credit;
+    if (notEnoughMoney) {
+      const err = new Error("You don't have enough money");
+      err.status = 400;
+      throw err;
     }
-    const err = new Error('Bad request');
-    err.status = 400;
-    throw err;
+    if (value > cardDoc.debit) {
+      cardDoc.credit += value - cardDoc.debit;
+      cardDoc.debit = 0;
+      cardDoc.save();
+      return cardDoc;
+    }
+    cardDoc.debit -= value;
+    await cardDoc.save();
+    return cardDoc;
   }
 
   setLimit(id, value) {
